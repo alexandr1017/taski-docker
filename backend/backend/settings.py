36423337d3b3afe5f26c8929c684aefce3ba2d2c
alexandr1017 <alexandr1993228@yandex.ru>
2026-07-21 21:43:54 +0300

    add setup file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','taski1017.hopto.org', '158.160.200.246']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'taski1017.hopto.org', '158.160.200.246']
 
 # Application definition
 
